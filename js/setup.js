22cae782b2adb3d0ea1454a3d9f1156153af4df7
andrex002 <sergeev-002@yandex.ru>
2020-07-11 10:22:40 +0300

    Задание 7. Исправил замечания

--- js/setup.js
+++ js/setup.js
@@ -54,17 +54,11 @@
   };
 
   var namesComparator = function (left, right) {
-    if (left > right) {
-      return 1;
-    } else if (left < right) {
-      return -1;
-    } else {
-      return 0;
-    }
+    return left - right;
   };
 
   var updateWizards = function () {
-    renderBlockWizards(wizards.sort(function (a, b) {
+    renderBlockWizards(wizards.slice().sort(function (a, b) {
       var rankDiff = getRank(b) - getRank(a);
       if (rankDiff === 0) {
         rankDiff = namesComparator(a.name, b.name);
